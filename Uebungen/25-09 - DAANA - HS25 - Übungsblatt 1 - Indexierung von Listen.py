03bb520224c394af2e1298626a9fc1c194dc7fb3
macnch33s3 <114980592+macnch33s3@users.noreply.github.com>
2025-09-19 11:44:54 +0200

    finished uebung 1

--- Uebungen/25-09 - DAANA - HS25 - Übungsblatt 1 - Indexierung von Listen.py
+++ Uebungen/25-09 - DAANA - HS25 - Übungsblatt 1 - Indexierung von Listen.py
@@ -13,15 +13,24 @@ df = pd.DataFrame(daten)
 #%% Aufgabe 1: Erster Nachname
 snn = df.sort_values(by=["Nachname"])
 print(snn)
-print(/n)
-
+print()
 #%% Aufgabe 2: Letzter Vorname
 svn = df.sort_values(by=["Vorname"])
 print(svn)
+print()
 #%% Aufgabe 3: Körpergrösse von Max
-
+print(df.loc[1, "Körpergrösse"])
+print()
 #%% Aufgabe 4: Zweitkleinster Nachname (alphabetisch)
-
+snn = df.sort_values(by=["Nachname"])
+print(df.loc[2, "Nachname"])
+print()
 #%% Aufgabe 5: Nachname der grössten Person
-
+# snng = df.sort_values(by="Körpergrösse")
+snng = df.loc[df["Körpergrösse"].idxmax(), "Nachname"]
+print(snng)
+print()
 #%% Aufgabe 6: Vorname der kleinsten Person
+svnk = df.loc[df["Körpergrösse"].idxmin(), "Vorname"]
+print(svnk)
+print()
